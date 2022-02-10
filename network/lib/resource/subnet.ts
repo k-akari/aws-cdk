edec1c2447ef323894b2806585668986aed92f48
k-akari <akari@canbright.co.jp>
2022-02-10 14:47:45 +0900

    [REMOVE] subnets in ap-northeast-1d

--- network/lib/resource/subnet.ts
+++ network/lib/resource/subnet.ts
@@ -31,12 +31,6 @@ export class Subnet extends Resource {
       availabilityZone: 'ap-northeast-1c',
       tags: [{ key: 'Name', value: this.createResourceName(scope, 'subnet-public-1c') }]
     })
-    this.subnetPublic1d = new CfnSubnet(scope, 'SubnetPublic1d', {
-      cidrBlock: '10.0.12.0/24',
-      vpcId: this.vpc.ref,
-      availabilityZone: 'ap-northeast-1d',
-      tags: [{ key: 'Name', value: this.createResourceName(scope, 'subnet-public-1d') }]
-    })
 
     // Private Subnets
     const subnetPrivate1a = new CfnSubnet(scope, 'SubnetPrivate1a', {
@@ -50,12 +44,6 @@ export class Subnet extends Resource {
       vpcId: this.vpc.ref,
       availabilityZone: 'ap-northeast-1c',
       tags: [{ key: 'Name', value: this.createResourceName(scope, 'subnet-private-1c') }]
-    })
-    const subnetPrivate1d = new CfnSubnet(scope, 'SubnetPrivate1d', {
-      cidrBlock: '10.0.102.0/24',
-      vpcId: this.vpc.ref,
-      availabilityZone: 'ap-northeast-1d',
-      tags: [{ key: 'Name', value: this.createResourceName(scope, 'subnet-private-1d') }]
     })
   }
 }
