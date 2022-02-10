cb23cd88fed069ef79318a8c6e7da0202edef308
k-akari <akari@canbright.co.jp>
2022-02-10 14:40:48 +0900

    [ADD] Internet Gateway

--- network/lib/network-stack.ts
+++ network/lib/network-stack.ts
@@ -2,13 +2,11 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Vpc } from './resource/vpc';
 import { Subnet } from './resource/subnet';
+import { InternetGateway } from './resource/internetGateway';
 
 export class NetworkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const envType = this.node.tryGetContext('envType');
-    const serviceName = this.node.tryGetContext('serviceName');
    
     // VPC
     const vpc = new Vpc();
@@ -17,5 +15,9 @@ export class NetworkStack extends Stack {
     // Subnet
     const subnet = new Subnet(vpc.vpc);
     subnet.createResources(this);
+
+    // Internet Gateway
+    const internetGateway = new InternetGateway(vpc.vpc);
+    internetGateway.createResources(this);
   }
 }
