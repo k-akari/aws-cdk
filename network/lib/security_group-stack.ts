fba51dbf20bdcd55a4339090f7d44b74cd93a1d8
k-akari <akari@canbright.co.jp>
2022-02-12 10:21:09 +0900

    [MOVE] security_group to network stack

---
+++ network/lib/security_group-stack.ts
@@ -0,0 +1,18 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { SecurityGroup } from './resource/securityGroup';
+import { NetworkStack } from '../../network/lib/network-stack';
+
+export class SecurityGroupStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const networkStack = new NetworkStack(scope, 'NetworkStack', {
+      stackName: 'network-stack'
+    });
+
+    // Security Group
+    const securityGroup = new SecurityGroup(networkStack.vpc);
+    securityGroup.createResources(this);
+  }
+}
