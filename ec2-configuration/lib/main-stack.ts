630893b79bf394932323201e86d53149fe4010e8
k-akari <akari@canbright.co.jp>
2022-02-15 22:39:35 +0900

    [REMOVE] unnecessary files

--- ec2-configuration/lib/main-stack.ts
+++ ec2-configuration/lib/main-stack.ts
@@ -2,8 +2,6 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NetworkStack } from './stack/network-stack';
 import { SecurityGroupStack } from './stack/security-group-stack';
-import { ContainerStack } from './stack/container-stack';
-import { DatabaseStack } from './stack/database-stack';
 import { SecretsManagerStack } from './stack/secrets-manager-stack';
 import { IamStack } from './stack/iam-stack';
 
@@ -23,16 +21,8 @@ export class MainStack extends Stack {
       stackName: 'iam-stack'
     });
 
-    new ContainerStack(scope, 'ContainerStack', networkStack.vpc, iamStack.iamRole, {
-      stackName: 'container-stack'
-    });
-
     const secretsManagerStack = new SecretsManagerStack(scope, 'SecretsManagerStack', {
       stackName: 'secrets-manager-stack'
-    });
-
-    new DatabaseStack(scope, 'DatabaseStack', networkStack.subnet, securityGroupStack.sg, secretsManagerStack.ssm, {
-      stackName: 'database-stack'
     });
   }
 }
