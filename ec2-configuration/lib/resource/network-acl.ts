805c21c4834d77f0a27b7c7cdb9133bf45b064c2
k-akari <akari@canbright.co.jp>
2022-02-19 18:07:15 +0900

    [REFACTOR] network acl

--- ec2-configuration/lib/resource/network-acl.ts
+++ ec2-configuration/lib/resource/network-acl.ts
@@ -4,45 +4,11 @@ import { Resource } from './abstract/resource';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Subnet } from './subnet';
 
-interface AssociationInfo {
-  readonly id: string;
-  readonly subnetId: () => string;
-}
-
-interface NetworkAclInfo {
-  readonly id: string;
-  readonly name: string;
-  readonly entryIdInbound: string;
-  readonly entryIdOutbound: string;
-  readonly associations: AssociationInfo[];
-  readonly assign: (networkAcl: CfnNetworkAcl) => void;
-}
-
 export class NetworkAcl extends Resource {
   public public: CfnNetworkAcl;
-  public private: CfnNetworkAcl;
 
   private readonly vpc: Vpc;
   private readonly subnet: Subnet;
-  private readonly networkAclInfos: NetworkAclInfo[] = [
-    {
-      id: 'NetworkAclPublic',
-      name: 'nacl-public',
-      entryIdInbound: 'NetworkAclEntryInboundPublic',
-      entryIdOutbound: 'NetworkAclEntryOutboundPublic',
-      associations: [
-        {
-          id: 'NetworkAclAssociationPublic1a',
-          subnetId: () => this.subnet.public1a.ref
-        },
-        {
-          id: 'NetworkAclAssociationPublic1c',
-          subnetId: () => this.subnet.public1c.ref
-        }
-      ],
-      assign: networkAcl => this.public = networkAcl
-    }
-  ];
 
   constructor(scope: Construct, vpc: Vpc, subnet: Subnet)
   {
@@ -51,42 +17,48 @@ export class NetworkAcl extends Resource {
     this.vpc = vpc;
     this.subnet = subnet;
 
-    for (const networkAclInfo of this.networkAclInfos) {
-      const networkAcl = this.createNetworkAcl(scope, networkAclInfo);
-      networkAclInfo.assign(networkAcl);
-    }
+    this.public = this.createPublicNetworkAcl(scope);
   }
 
-  private createNetworkAcl(scope: Construct, networkAclInfo: NetworkAclInfo): CfnNetworkAcl {
-    const networkAcl = new CfnNetworkAcl(scope, networkAclInfo.id, {
+  private createPublicNetworkAcl(scope: Construct): CfnNetworkAcl {
+    // Create Network ACL
+    const networkAcl = new CfnNetworkAcl(scope, 'NetworkAclPublic', {
       vpcId: this.vpc.vpcId,
-      tags: [{
-        key: 'Name', value: this.createResourceName(scope, networkAclInfo.name)
-      }]
+      tags: [{ key: 'Name', value: this.createResourceName(scope, 'nacl-public')}]
     });
-    this.createEntry(scope, networkAclInfo.entryIdInbound, networkAcl, false);
-    this.createEntry(scope, networkAclInfo.entryIdOutbound, networkAcl, true);
-    for (const associationInfo of networkAclInfo.associations) {
-      this.createAssociation(scope, associationInfo, networkAcl);
-    }
-    return networkAcl;
-  }
 
-  private createEntry(scope: Construct, id: string, networkAcl: CfnNetworkAcl, egress: boolean) {
-    new CfnNetworkAclEntry(scope, id, {
+    // Create Inbound Rules and Associate them with Network ACL
+    new CfnNetworkAclEntry(scope, 'NetworkAclEntryInboundPublic', {
       networkAclId: networkAcl.ref,
       protocol: -1,
       ruleAction: 'allow',
       ruleNumber: 100,
       cidrBlock: '0.0.0.0/0',
-      egress: egress
+      egress: false
     });
-  }
 
-  private createAssociation(scope: Construct, associationInfo: AssociationInfo, networkAcl: CfnNetworkAcl) {
-    new CfnSubnetNetworkAclAssociation(scope, associationInfo.id, {
+    // Create Outbound Rules and Associate them with Network ACL
+    new CfnNetworkAclEntry(scope, 'NetworkAclEntryOutboundPublic', {
       networkAclId: networkAcl.ref,
-      subnetId: associationInfo.subnetId()
+      protocol: -1,
+      ruleAction: 'allow',
+      ruleNumber: 100,
+      cidrBlock: '0.0.0.0/0',
+      egress: true
     });
+
+    // Associate the created Network ACL with public subnet of 1a
+    new CfnSubnetNetworkAclAssociation(scope, 'NetworkAclAssociationPublic1a', {
+      networkAclId: networkAcl.ref,
+      subnetId: this.subnet.public1a.ref
+    });
+
+    // Associate the created Network ACL with public subnet of 1c
+    new CfnSubnetNetworkAclAssociation(scope, 'NetworkAclAssociationPublic1c', {
+      networkAclId: networkAcl.ref,
+      subnetId: this.subnet.public1c.ref
+    });
+
+    return networkAcl;
   }
 }
