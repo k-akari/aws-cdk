23bd57c030c6d13537557212ff39c1560d83e056
k-akari <akari@canbright.co.jp>
2022-02-19 18:35:07 +0900

    [REFACTOR] security-group

--- ec2-configuration/lib/resource/security-group.ts
+++ ec2-configuration/lib/resource/security-group.ts
@@ -1,149 +1,80 @@
 import { Construct } from 'constructs';
-import { CfnSecurityGroup, CfnSecurityGroupIngress, CfnSecurityGroupIngressProps, CfnSecurityGroupEgress } from 'aws-cdk-lib/aws-ec2';
+import { CfnSecurityGroup, CfnSecurityGroupIngress, CfnSecurityGroupEgress } from 'aws-cdk-lib/aws-ec2';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Resource } from './abstract/resource';
-
-interface IngressInfo {
-  readonly id: string;
-  readonly securityGroupIngressProps: CfnSecurityGroupIngressProps;
-  readonly groupId: () => string;
-  readonly sourceSecurityGroupId?: () => string;
-}
-
-interface EgressInfo {
-  readonly id: string;
-  readonly ipProtocol: string;
-  readonly cidrIp: string;
-  readonly fromPort: number;
-  readonly toPort: number;
-  readonly groupId: () => string;
-  readonly sourceSecurityGroupId?: () => string;
-}
-
-interface SecurityGroupInfo {
-  readonly id: string;
-  readonly groupDescription: string;
-  readonly ingresses: IngressInfo[];
-  readonly egresses?: EgressInfo[];
-  readonly resourceName: string;
-  readonly assign: (securityGroup: CfnSecurityGroup) => void;
-}
 
 export class SecurityGroup extends Resource {
   public alb: CfnSecurityGroup;
   public ec2: CfnSecurityGroup;
 
   private readonly vpc: Vpc;
-  private readonly securityGroupInfos: SecurityGroupInfo[] = [
-    {
-      id: 'SecurityGroupAlb',
-      groupDescription: 'for ALB',
-      ingresses: [
-        {
-          id: 'SecurityGroupIngressAlb1',
-          securityGroupIngressProps: {
-            ipProtocol: 'tcp',
-            cidrIp: '0.0.0.0/0',
-            fromPort: 80,
-            toPort: 80
-          },
-          groupId: () => this.alb.attrGroupId
-        },
-        {
-          id: 'SecurityGroupIngressAlb2',
-          securityGroupIngressProps: {
-            ipProtocol: 'tcp',
-            cidrIp: '0.0.0.0/0',
-            fromPort: 443,
-            toPort: 443
-          },
-          groupId: () => this.alb.attrGroupId
-        }
-      ],
-      resourceName: 'sg-alb',
-      assign: securityGroup => this.alb = securityGroup
-    },
-    {
-      id: 'SecurityGroupEc2',
-      groupDescription: 'for EC2',
-      ingresses: [
-        {
-          id: 'SecurityGroupIngressEc21',
-          securityGroupIngressProps: {
-            ipProtocol: 'tcp',
-            fromPort: 80,
-            toPort: 80
-          },
-          groupId: () => this.ec2.attrGroupId,
-          sourceSecurityGroupId: () => this.alb.attrGroupId,
-        }
-      ],
-      egresses: [
-        {
-          id: 'SecurityGroupEgressEc21',
-          ipProtocol: 'tcp',
-          cidrIp: '0.0.0.0/0',
-          fromPort: 443,
-          toPort: 443,
-          groupId: () => this.ec2.attrGroupId
-        }
-      ],
-      resourceName: 'sg-ec2',
-      assign: securityGroup => this.ec2 = securityGroup
-    }
-  ];
 
   constructor(scope: Construct, vpc: Vpc) {
     super();
 
     this.vpc = vpc;
 
-    for (const securityGroupInfo of this.securityGroupInfos) {
-      const securityGroup = this.createSecurityGroup(scope, securityGroupInfo);
-      securityGroupInfo.assign(securityGroup);
-
-      this.createSecurityGroupIngress(scope, securityGroupInfo);
-      this.createSecurityGroupEgress(scope, securityGroupInfo);
-    }
+    this.alb = this.createAlbSecurityGroup(scope);
+    this.ec2 = this.createEc2SecurityGroup(scope);
   };
 
-  private createSecurityGroup(scope: Construct, securityGroupInfo: SecurityGroupInfo): CfnSecurityGroup {
-    const securityGroupName = this.createResourceName(scope, securityGroupInfo.resourceName);
-    const securityGroup = new CfnSecurityGroup(scope, securityGroupInfo.id, {
-      groupDescription: securityGroupInfo.groupDescription,
+  private createAlbSecurityGroup(scope: Construct): CfnSecurityGroup {
+    // Create Security Group
+    const securityGroupName = this.createResourceName(scope, 'sg-alb');
+    const securityGroup = new CfnSecurityGroup(scope, 'SecurityGroupAlb', {
+      groupDescription: 'for ALB',
       groupName: securityGroupName,
       vpcId: this.vpc.vpcId,
-      tags: [{
-        key: 'Name',
-        value: securityGroupName
-      }]
+      tags: [{ key: 'Name', value: securityGroupName }]
+    });
+
+    // Create Ingress Rules and Associate them with the Security Group
+    new CfnSecurityGroupIngress(scope, 'SecurityGroupIngressAlb1', {
+      ipProtocol: 'tcp',
+      cidrIp: '0.0.0.0/0',
+      fromPort: 80,
+      toPort: 80,
+      groupId: securityGroup.attrGroupId
+    });
+    new CfnSecurityGroupIngress(scope, 'SecurityGroupIngressAlb2', {
+      ipProtocol: 'tcp',
+      cidrIp: '0.0.0.0/0',
+      fromPort: 443,
+      toPort: 443,
+      groupId: securityGroup.attrGroupId
     });
 
     return securityGroup;
   }
 
-  private createSecurityGroupIngress(scope: Construct, securityGroupInfo: SecurityGroupInfo) {
-    for (const ingress of securityGroupInfo.ingresses) {
-      const securityGroupIngress = new CfnSecurityGroupIngress(scope, ingress.id, ingress.securityGroupIngressProps);
-      securityGroupIngress.groupId = ingress.groupId();
+  private createEc2SecurityGroup(scope: Construct): CfnSecurityGroup {
+    // Create Security Group
+    const securityGroupName = this.createResourceName(scope, 'sg-ec2');
+    const securityGroup = new CfnSecurityGroup(scope, 'SecurityGroupEc2', {
+      groupDescription: 'for EC2',
+      groupName: securityGroupName,
+      vpcId: this.vpc.vpcId,
+      tags: [{ key: 'Name', value: securityGroupName }]
+    });
 
-      if (ingress.sourceSecurityGroupId) {
-        securityGroupIngress.sourceSecurityGroupId = ingress.sourceSecurityGroupId();
-      }
-    }
-  }
+    // Create a Ingress Rule and Associate it with the Security Group
+    new CfnSecurityGroupIngress(scope, 'SecurityGroupIngressEc21', {
+      ipProtocol: 'tcp',
+      fromPort: 80,
+      toPort: 80,
+      groupId: securityGroup.attrGroupId,
+      sourceSecurityGroupId: this.alb.attrGroupId
+    });
 
-  private createSecurityGroupEgress(scope: Construct, securityGroupInfo: SecurityGroupInfo) {
-    if (securityGroupInfo.egresses) {
-      for (const egress of securityGroupInfo.egresses) {
-        new CfnSecurityGroupEgress(scope, egress.id, {
-          ipProtocol: egress.ipProtocol,
-          cidrIp: egress.cidrIp,
-          fromPort: egress.fromPort,
-          toPort: egress.toPort,
-          groupId: this.ec2.attrGroupId
-        });
-      }
-    }
+    // Create a Egress Rule and Associate it with the Security Group
+    new CfnSecurityGroupEgress(scope, 'SecurityGroupEgressEc21', {
+      ipProtocol: 'tcp',
+      cidrIp: '0.0.0.0/0',
+      fromPort: 443,
+      toPort: 443,
+      groupId: securityGroup.attrGroupId
+    });
+
+    return securityGroup;
   }
 }
